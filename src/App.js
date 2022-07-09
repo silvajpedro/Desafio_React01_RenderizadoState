da8fcab15afaee64405b6ab81df30582f0f7dd95
silvajpedro <belojpedro.31@gmail.com>
2022-07-09 15:12:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React, {Component} from "react";
+
+export default class Coisa extends Component{
+state = {
+  nome:"João",
+  idade:19,
+  comidafav:"Empadão",
+  musicas:["My Way","How Can I Go On","Love Me Like There´s No Tomorrow"]
+}
+ render(){ 
+  return(
+    <div>
+    <h1>{this.state.nome}</h1>
+    <h2>{this.state.idade}</h2>
+    <h3>{this.state.comidafav}</h3>
+    <ul>
+      <li>{this.state.musicas[0]}</li>
+      <li>{this.state.musicas[1]}</li>
+      <li>{this.state.musicas[2]}</li>
+    </ul>
+    </div>
+  )
+}
+}
